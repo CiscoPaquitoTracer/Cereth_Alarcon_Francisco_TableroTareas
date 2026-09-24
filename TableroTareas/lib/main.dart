ab3cd3ee26bdc46b2c79af181a7862e5e94d31d3
CiscoPaquitoTracer <20233tn239@utez.edu.mx>
2026-09-23 20:03:39 -0600

    [ADD]Se agrego funcionalidades requeridas, tachar tareas marcadas, agregar y eliminar tareas

--- TableroTareas/lib/main.dart
+++ TableroTareas/lib/main.dart
@@ -11,111 +11,156 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Tareas',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(seedColor: Colors.teal),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const ListaVivaPage(title: 'Cereth 10A - Tareas'), //97 y 115
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class ListaVivaPage extends StatefulWidget {
+  const ListaVivaPage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ListaVivaPage> createState() => _ListaVivaPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ListaVivaPageState extends State<ListaVivaPage> {
+  final List<String> _tareas = ['Tarea Investigacion', 'Subir tarea cris'];
+  final List<bool> _completadas = [false, false];
 
-  void _incrementCounter() {
+  bool _soloPendientes = true;
+  bool _soloCompletadas = false;
+
+  int _siguienteTarea = 3;
+
+  void _cambiarEstado(int index, bool valor) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _completadas[index] = valor;
+    });
+  }
+
+  void _eliminar(int index) {
+    setState(() {
+      _tareas.removeAt(index);
+    });
+  }
+
+  void _agregar() {
+    setState(() {
+      _tareas.add('Tarea $_siguienteTarea');
+      _completadas.add(false);
+      _siguienteTarea++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final totalCompletadas =
+        _completadas.where((completada) => completada).length;
+
+    final indicesVisibles = List<int>.generate(
+      _tareas.length,
+          (index) => index,
+    ).where((index) {
+      return !_soloPendientes || !_completadas[index];
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    'Completadas: $totalCompletadas / ${_tareas.length}',
+                  ),
+                ),
+                const Text(
+                  'Solo pendientes',
+                  style: TextStyle(fontSize: 13),
+                ),
+                const SizedBox(width: 4),
+                Switch(
+                  value: _soloPendientes,
+                  onChanged: (valor) {
+                    setState(() {
+                      _soloPendientes = valor;
+                    });
+                  },
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          const Divider(height: 1),
+          Expanded(
+            child: indicesVisibles.isEmpty
+                ? Center(
+              child: Text(
+                _tareas.isEmpty
+                    ? 'Agrega tu primera tarea'
+                    : 'No hay tareas pendientes',
+              ),
+            )
+                : ListView.builder(
+              padding: const EdgeInsets.only(bottom: 96),
+              itemCount: indicesVisibles.length,
+              itemBuilder: (context, posicion) {
+                final index = indicesVisibles[posicion];
+
+                return Card(
+                  elevation: 1,
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 6,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: ListTile(
+                    leading: Checkbox(
+                      value: _completadas[index],
+                      onChanged: (valor) {
+                        if (valor != null) {
+                          _cambiarEstado(index, valor);
+                        }
+                      },
+                    ),
+                    title: Text(
+                      _tareas[index],
+                      style: TextStyle(
+                        fontSize: 14,
+                        decoration: _completadas[index]
+                            ? TextDecoration.lineThrough
+                            : TextDecoration.none,
+                      ),
+                    ),
+                    trailing: IconButton(
+                      tooltip: 'Eliminar tarea',
+                      onPressed: () => _eliminar(index),
+                      icon: const Icon(Icons.delete_outline),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _agregar,
+        icon: const Icon(Icons.add_task_rounded),
+        label: const Text('Agregar'),
       ),
     );
   }
